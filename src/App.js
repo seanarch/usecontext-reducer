48e22517af0f446236d6dc09aa0c7e76077799f5
seanarch <zhuqingyun@outlook.com>
2023-06-07 17:15:23 -0600

    Use multiple reducers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import CounterOne from "./component/CounterOne";
 import CounterTwo from "./component/CounterTwo";
+import CounterThree from "./component/CounterThree";
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne /> */}
-      <CounterTwo />
+      {/* <CounterTwo /> */}
+      <CounterThree />
     </div>
   );
 }
