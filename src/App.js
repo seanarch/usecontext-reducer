5cead5347427b6e8f87ecbc8f99cf32b10992675
seanarch <zhuqingyun@outlook.com>
2023-06-07 16:54:15 -0600

    Use action type and two counters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CounterOne from "./component/CounterOne";
+import CounterTwo from "./component/CounterTwo";
 
 function App() {
   return (
     <div className="App">
-      <CounterOne />
+      {/* <CounterOne /> */}
+      <CounterTwo />
     </div>
   );
 }
