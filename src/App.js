7ca2c0e0446af7c0ecf9425bff7f11ff9e8f7ee0
seanarch <zhuqingyun@outlook.com>
2023-06-07 16:48:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CounterOne from "./component/CounterOne";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
